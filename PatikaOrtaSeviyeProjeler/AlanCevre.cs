af1b5cdbcbcd8a0132ff861be8100f5ebd23a101
Ömer Ulutaş <omerulutas@outlook.com>
2023-04-06 20:56:42 +0300

    Add project files.

---
+++ PatikaOrtaSeviyeProjeler/AlanCevre.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOrtaSeviyeProjeler
+{
+    class AlanCevre
+    {
+        public static void DaireHesap()
+        {
+            Console.WriteLine("Neyi hesaplamak istersin;\n1. Alan\n2. Çevre");
+            string secim = Console.ReadLine();
+            double alan = 0;
+            double cevre = 0;
+
+            if (secim == "1")
+            {
+                Console.Write("Alan hesaplamak için dairenin yarıçapını yazınız: ");
+                double yarıcap = double.Parse(Console.ReadLine());
+                alan = Math.PI * (yarıcap * yarıcap);
+                Console.WriteLine($"Dairenin alanı ==> {alan}");
+            }
+            else if (secim == "2")
+            {
+                Console.Write("Çevre hesaplamak için dairenin yarıçapını yazınız: ");
+                double yarıcap = double.Parse(Console.ReadLine());
+                cevre = 2 * Math.PI * yarıcap;
+                Console.WriteLine($"Dairenin cevresi ==> {cevre}");
+            }
+        }
+        public static void UcgenHesap()
+        {
+            Console.WriteLine("Neyi hesaplamak istersin;\n1. Alan\n2. Çevre");
+            string secim = Console.ReadLine();
+            double alan = 0;
+            int cevre = 0;
+
+            if (secim == "1")
+            {
+                Console.Write("Alan hesaplamak için üçgenin yüksekliğini yazınız: ");
+                double yukseklik = double.Parse(Console.ReadLine());
+                Console.Write("Üçgenin uzunluğunu yazınız: ");
+                double uzunluk = double.Parse(Console.ReadLine());
+                alan = (uzunluk * yukseklik) / 2;
+                Console.WriteLine($"Üçgenin alanı ==> {alan}");
+            }
+            else if (secim == "2")
+            {
+                Console.Write("Çevre hesaplamak için üçgenin kenar uzunluklarını yazınız: ");
+                int kenar1 = int.Parse(Console.ReadLine());
+                int kenar2 = int.Parse(Console.ReadLine());
+                int kenar3 = int.Parse(Console.ReadLine());
+                cevre = kenar1 + kenar2 + kenar3;
+                Console.WriteLine($"Üçgenin cevresi ==> {cevre}");
+            }
+        }
+        public static void KareHesap()
+        {
+            Console.WriteLine("Neyi hesaplamak istersin;\n1. Alan\n2. Çevre");
+            string secim = Console.ReadLine();
+            double alan = 0;
+            int cevre = 0;
+
+            if (secim == "1")
+            {
+                Console.Write("Alan hesaplamak için karenin bir kenar uzunluğunu yazınız: ");
+                double uzunluk = double.Parse(Console.ReadLine());
+                alan = uzunluk * uzunluk;
+                Console.WriteLine($"Karenin alanı ==> {alan}");
+            }
+            else if (secim == "2")
+            {
+                Console.Write("Çevre hesaplamak için karenin kenar uzunluklarını yazınız: ");
+                int uzunluk = int.Parse(Console.ReadLine());
+                cevre = uzunluk * 4;
+                Console.WriteLine($"Karenin cevresi ==> {cevre}");
+            }
+        }
+        public static void DikdörtgenHesap()
+        {
+            Console.WriteLine("Neyi hesaplamak istersin;\n1. Alan\n2. Çevre");
+            string secim = Console.ReadLine();
+            double alan = 0;
+            int cevre = 0;
+
+            if (secim == "1")
+            {
+                Console.Write("Alan hesaplamak için dikdörtgenin geniş kenarı yazınız: ");
+                int yukseklik = int.Parse(Console.ReadLine());
+                Console.Write("Uzun kenar: ");
+                int uzunluk = int.Parse(Console.ReadLine());
+                alan = yukseklik * uzunluk;
+                Console.WriteLine($"Dikdörtgenin alanı ==> {alan}");
+            }
+            else if (secim == "2")
+            {
+                Console.Write("Alan hesaplamak için dikdörtgenin geniş kenarı yazınız: ");
+                int yukseklik = int.Parse(Console.ReadLine());
+                Console.Write("Uzun kenar: ");
+                int uzunluk = int.Parse(Console.ReadLine());
+                cevre = (yukseklik * 2) * (uzunluk * 2);
+                Console.WriteLine($"Dikdörtgenin cevresi ==> {cevre}");
+            }
+        }
+    }
+}
---
+++ PatikaOrtaSeviyeProjeler/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOrtaSeviyeProjeler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //AlanveCevreHesaplama();
+            //NInteger();
+            //MutlakKareAlma();
+            //KarakterDegistirme();
+            SessizHarf();
+            Console.ReadLine();
+        }
+
+        private static void SessizHarf()
+        {
+            Console.Write("Metin Giriniz: ");
+            string metin = Console.ReadLine().ToLower();
+            string[] metinler = metin.Split(' ');
+            string sessiz = ("bcçdfgğhjklmnprsştvyz");
+            char[] sessizler = sessiz.ToCharArray();
+
+            foreach (var item in metinler)
+            {
+                bool sessizMi = false;
+                for (int i = 0; i < item.Length - 1; i++)
+                {
+                    if (sessizler.Contains(item[i]) && sessizler.Contains(item[i + 1]))
+                    {
+                        sessizMi = true; 
+                        break;
+                    }
+                }
+                Console.Write(sessizMi + " ");
+            }
+        }
+
+        private static void KarakterDegistirme()
+        {
+            Console.WriteLine("Lütfen bir şeyler yazın: ");
+            string input = Console.ReadLine();
+            string[] inputWords = input.Split(' ');
+
+            for (int i = 0; i < inputWords.Length; i++)
+            {
+                string word1 = inputWords[i].Remove(0, 1);
+
+                Console.Write($"{word1}" + $"{inputWords[i][0]}" + " ");
+            };
+        }
+
+        private static void MutlakKareAlma()
+        {
+            Console.Write("Lütfen aralarında boşluk bırakarak 'n' adet sayı giriniz: ");
+            string[] input = Console.ReadLine().Split(' ');
+            int sum = 0;
+            int sum2 = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (int.Parse(input[i]) < 67)
+                {
+                    sum += 67 - int.Parse(input[i]);
+                }
+                else
+                {
+                    int sub = Math.Abs(67 - int.Parse(input[i]));
+                    sum2 += sub * sub;
+                }
+            }
+            Console.WriteLine($"{sum} {sum2}");
+        }
+
+        private static void NInteger()
+        {
+            Console.Write("Lütfen aralarında boşluk bırakarak 'n' adet ikili tamsayı giriniz: ");
+            string[] input = Console.ReadLine().Split(' ');
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                int num1 = int.Parse(input[i]);
+                int num2 = int.Parse(input[i + 1]);
+
+                sum += num1 + num2;
+
+                if (num1 == num2)
+                {
+                    Console.Write(sum * sum + " ");
+                }
+                else
+                {
+                    Console.Write(sum + " ");
+                }
+            }
+
+        }
+
+        private static void AlanveCevreHesaplama()
+        {
+            Console.WriteLine("\t\tAlan-Çevre Hesaplama Programı");
+            Console.WriteLine("Lütfen hesaplama yapmak istediğiniz şekli seçiniz:\n1. Daire");
+            Console.WriteLine("2. Üçgen\n3. Kare\n4. Dikdörtgen");
+            Console.Write("Lütfen seçimi yapınız: ");
+            string secim = Console.ReadLine();
+
+            if (secim == "1")
+            {
+                AlanCevre.DaireHesap();
+            }
+            else if (secim == "2")
+            {
+                AlanCevre.UcgenHesap();
+            }
+            else if (secim == "3")
+            {
+                AlanCevre.KareHesap();
+            }
+            else if (secim == "4")
+            {
+                AlanCevre.DikdörtgenHesap();
+            }
+            else
+            {
+                Console.WriteLine("Yanlış giriş yaptınız!!!");
+            }
+        }
+    }
+}
